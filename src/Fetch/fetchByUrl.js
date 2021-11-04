ea2ca4ab9c1b08a872172e131ad06608f84f85fd
Anas Tazir <atazir2018@gmail.com>
2021-11-04 23:22:10 +0530

    edit headers

--- src/Fetch/fetchByUrl.js
+++ src/Fetch/fetchByUrl.js
@@ -23,7 +23,7 @@ export const handleUrl = (text, coordinates, setNotifications, add, style, notif
       mode: 'cors',
       credentials: 'include',
       headers: {
-        'Access-Control-Allow-Origin':'https://tf-image-classification-server.herokuapp.com',
+        'Access-Control-Allow-Origin':'https://frosty-varahamihira-2e4ee4.netlify.app',
         'Access-Control-Allow-Methods':'GET, POST, PATCH, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers':'Origin, Content-Type, X-Auth-Token',
       }
@@ -74,7 +74,7 @@ export const handleUpload=(uploadedImage, coordinates, style, add, notifications
         body: formData,
         mode: 'cors',
         headers: {
-          'Access-Control-Allow-Origin':'https://tf-image-classification-server.herokuapp.com',
+          'Access-Control-Allow-Origin':'https://frosty-varahamihira-2e4ee4.netlify.app',
           'Access-Control-Allow-Methods':'GET, POST, PATCH, PUT, DELETE, OPTIONS',
           'Access-Control-Allow-Headers':'Origin, Content-Type, X-Auth-Token',
         }
