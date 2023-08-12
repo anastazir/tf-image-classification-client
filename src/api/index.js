5960dd9fb83084d49492985abae8934b7395989a
Anas Tazir <atazir2018@gmail.com>
2023-08-12 20:54:33 +0530

    update backend api

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://tf-image-classification-server.herokuapp.com' });
+// const API = axios.create({ baseURL: 'your-api-link' });
+const API = axios.create({ baseURL: process.env.API_GATEWAY_API });
 
-export const predict_image = (formData, style) => API.post(`/urlRoute/${style}`, formData);
+export const predict_image = (formData, style) => API.post(`/Prod/urlRoute/${style}`, formData);
 
-export const predict_file = (fromData, style) => API.post(`/upload-image/${style}`, fromData);
+export const predict_file = (fromData, style) => API.post(`/Prod/upload-image/${style}`, fromData);
 
-export const fetch_labels = (style) => API.get(`/fetchLabels?labelsType=${style}`)
+export const fetch_labels = (style) => API.get(`/Prod/fetchLabels?labelsType=${style}`)
