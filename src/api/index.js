74758fa2e9e33e91baf25a55d37e52a0bc4b0a38
Anas Tazir <atazir2018@gmail.com>
2023-08-14 22:44:36 +0530

    update predict_file api

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,6 @@ const API = axios.create({ baseURL: process.env.REACT_APP_API_GATEWAY_API });
 
 export const predict_image = (formData, style) => API.post(`/Prod/urlRoute/${style}`, formData);
 
-export const predict_file = (fromData, style) => API.post(`/Prod/upload-image/${style}`, fromData);
+export const predict_file = (fromData, style) => API.post(`/Prod/urlRoute/${style}`, fromData);
 
 export const fetch_labels = (style) => API.get(`/Prod/fetchLabels?labelsType=${style}`)
