42003a3bfb7656622c575a4c40c12a26a127ff1e
Anas Tazir <atazir2018@gmail.com>
2023-08-24 12:22:21 +0530

    only call production api

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // const API = axios.create({ baseURL: 'your-api-link' });
-const API = axios.create({ baseURL: process.env.REACT_APP_API_GATEWAY_API });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_GATEWAY_API + "/Prod"});
 
-export const predict_image = (formData, style) => API.post(`/Prod/urlRoute/${style}`, formData);
+export const predict_image = (formData, style) => API.post(`/urlRoute/${style}`, formData);
 
-export const predict_file = (fromData, style) => API.post(`/Prod/urlRoute/${style}`, fromData);
+export const predict_file = (fromData, style) => API.post(`/urlRoute/${style}`, fromData);
 
-export const fetch_labels = (style) => API.get(`/Prod/fetchLabels?labelsType=${style}`)
+export const fetch_labels = (style) => API.get(`/fetchLabels?labelsType=${style}`)
