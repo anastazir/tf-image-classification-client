329925914d08dad884b014d29eac4a73f2bc3342
anastazir <atazir2018@gmail.com>
2023-10-27 02:21:24 +0530

    edit header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
     <motion.h1 style={{textAlign: 'center'}} className="pink">
         Tensorflow
         <span className="light-blue"> Project </span>
-        <span>
+        {/* <span>
           <a target="_blank" style={{ backgroundColor: "black"}} href="https://github.com/anastazir/tensorflow-image-classification" rel="noreferrer">
             <img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" />
           </a>
-        </span>
+        </span> */}
     </motion.h1>
   )
 }
